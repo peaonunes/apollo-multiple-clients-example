24b5b724c314fc8f67707849d6eba945c1177049
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2018-08-06 23:20:53 -0300

    feat(App): use apollo and create two clients example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+
+import ApolloClient from "apollo-boost";
+
+import gql from "graphql-tag";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,14 +14,108 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <p className="App-intro">
+            Two Apollo clients consuming two different GraphQL APIs. First using
+            context client, second using props client.
+          </p>
+          <div className="App-container">
+            <div className="App-container-item" key="1">
+              <QueryDefaultClient />
+            </div>
+            <div className="App-container-item" key="2">
+              <QueryOverridingClient />
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
 }
+
+const storeQuery = gql`
+  query {
+    stores {
+      edges {
+        name
+        id
+      }
+      totalCount
+    }
+  }
+`;
+
+const QueryDefaultClient = () => {
+  return (
+    <Query query={storeQuery}>
+      {({ error, loading, data }) => {
+        if (error) return "Error!";
+        if (loading) return "Loading!";
+
+        if (data) {
+          const stores = data.stores;
+          return (
+            <ul>
+              <h3>Default client providing stores</h3>
+              <p>{`${stores.totalCount} stores!`}</p>
+              {stores.edges.map(store => {
+                return (
+                  <li key={`${store.name}-${store.id}`}>{`Name: ${
+                    store.name
+                  } - Id: ${store.id}`}</li>
+                );
+              })}
+            </ul>
+          );
+        }
+      }}
+    </Query>
+  );
+};
+
+const cityQuery = gql`
+  query {
+    cities {
+      edges {
+        name
+        id
+      }
+      totalCount
+    }
+  }
+`;
+
+const customClient = new ApolloClient({
+  uri: "http://localhost:3002/graphql"
+});
+
+const QueryOverridingClient = () => {
+  return (
+    <Query query={cityQuery} client={customClient}>
+      {({ error, loading, data }) => {
+        if (error) return "Error!";
+        if (loading) return "Loading!";
+
+        if (data) {
+          const cities = data.cities;
+          return (
+            <ul>
+              <h3>Overriding client through props providing cities</h3>
+              <p>{`${cities.totalCount} cities!`}</p>
+              {cities.edges.map(city => {
+                return (
+                  <li key={`${city.name}-${city.id}`}>{`Name: ${
+                    city.name
+                  } - Id: ${city.id}`}</li>
+                );
+              })}
+            </ul>
+          );
+        }
+      }}
+    </Query>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import "./index.css";
+import App from "./App";
+
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:3001/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={defaultClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
